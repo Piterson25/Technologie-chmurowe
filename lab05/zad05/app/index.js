7fd72bca756975f93d469253e12941dabe71b68b
Piterson25 <piotrmaszczak25@gmail.com>
2023-03-29 20:06:15 +0200

    zad05

--- lab05/zad05/app/index.js
+++ lab05/zad05/app/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send(`Hello World!`);
-});
-
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(port, () => {
-  console.log(`Nasluchiwanie na porcie ${port}`);
+  console.log(`Server is running on port ${port}.`);
 });
